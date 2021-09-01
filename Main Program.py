28c99a47b1b0f8cc87b4d64aacb7b577310df015
JerryLiu0911 <89786918+JerryLiu0911@users.noreply.github.com>
2021-09-01 21:40:54 +0100

    Create Main Program.py

---
+++ Main Program.py
@@ -0,0 +1,12 @@
+import turtle
+arrow=turtle.Turtle()
+for x in range(0,10):
+  arrow.left(20)
+  arrow.forward(80)
+  arrow.left(52)
+  arrow.forward(80)
+  arrow.left(128)
+  arrow.forward(80)
+  arrow.left(52)
+  arrow.forward(80)
+turtle.done()
